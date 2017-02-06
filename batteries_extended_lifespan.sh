45515816c0da446f8b9e764a9f59a84fa5c1f32b
SamageZA <samageza@pikolec.co.za>
2017-02-06 22:20:17 +0200

    Added links to the tpl scripts PPA for ubuntu

--- batteries_extended_lifespan.sh
+++ batteries_extended_lifespan.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# sudo add-apt-repository ppa:morgwai/tpbat
+# sudo apt-get update && sudo apt-get install acpi tpacpi-bat tpbat-utils-acpi
+
+#
+# This script updates the tlp thresholds for the battery to 40%/70% to maximise battery lifespan.
+#
 if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user." 2>&1
   exit 1
--- batteries_fullcharge.sh
+++ batteries_fullcharge.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# sudo add-apt-repository ppa:morgwai/tpbat
+# sudo apt-get update && sudo apt-get install acpi tpacpi-bat tpbat-utils-acpi tlp
+#
+# This script updates the tlp thresholds for the battery to 96%/100% to maximise battery capacity
+#
 if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user." 2>&1
   exit 1
